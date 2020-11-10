85c791a402b168b57ef4b3b45d75914a06c43c02
Imran51 <imransayeed26@gmail.com>
2020-11-10 10:42:24 +0600

    feat: Express framework install and implemented

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const express = require('express');
 const http = require('http');
+const logger = require('morgan');
 
 const hostName = 'localhost';
 const port = 3000;
-const server = http.createServer((req,res) => {
-    console.log(req.headers);
 
+const app = express();
+app.use(logger('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Hello, Node Application one!</h1></body></html>');
-
+    res.end('<html><body><h1>Hello, This is Express Server</h1></body></html>');
 });
+
+const server = http.createServer(app);
 
 server.listen(port,hostName, () => {
     console.log(`Server running at http://${hostName}:${port}`);
